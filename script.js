f7e3faa4d7e480a9768618a91cb08fb88fb5ecd4
Nicolas Vaagen <nicolasvaagen@gmail.com>
2023-10-28 09:22:13 -0600

    added file upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let buttons = document.querySelector('button');
-let urlEE = "https://lichess.org/study/embed/okZJcfZb/5JWAfEdt"
-
+let mainBoard = 
 
 //change the src for the chess board
 function changeBoard(element) {
+  //get SRC from the html tag "data-url"
   const SRC = element.dataset.url;
-  document.getElementById("board").src = SRC;
+  mainBoard.src = SRC;
 }
 
 class ExampleGame extends HTMLButtonElement {
@@ -13,18 +12,12 @@ class ExampleGame extends HTMLButtonElement {
     super(this.innerHTML);
     this.addEventListener('click', changeBoard(this))
   }
-  getURL() {
-    alert("game");
-  }
 }
 
 class Line extends HTMLButtonElement {
   constructor(){
     super(this.innerHTML);
     this.addEventListener('click', changeBoard(this))
-  }
-  getURL(){
-    alert("line");
   }
 }
 
