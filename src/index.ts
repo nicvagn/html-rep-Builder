3b14fa7c7c6863abcb112de6b67391e484d15482
Nicolas Vaagen <nicolasvaagen@gmail.com>
2023-12-25 16:40:50 -0600

    Version 1.1. Save controller is much better.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
+/*********************************************************************************
+ * version 1.1
+ *
+ * a typescript chess repertoire builder. including line and example game viewing.
+ *
+ * made for shcc: Saskatchewan Horizon Chess Club
+ * Copyright (C) 2023 Nicolas Vaagen
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************************/
+
 import { Controller } from "./repertoire-controller.mjs";
 import { EditRepertoireController } from "./edit-repertoire-controller.mjs";
 import { load } from "./save-control";
